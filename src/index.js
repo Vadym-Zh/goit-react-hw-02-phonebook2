5e5bed5759276233472eda2830c66a0fc65aa059
Vadym Zhuravlov <vadiazh@gmail.com>
2023-06-09 22:35:42 +0300

    1

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import { nanoid } from 'nanoid';
+
+import data from './data/contacts';
+
+const contacts = data.map(contact => {
+  contact.id = nanoid();
+  return contact;
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App initialContacts={contacts} />
   </React.StrictMode>
 );
